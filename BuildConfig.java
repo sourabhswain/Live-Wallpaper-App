90d04c09f243bf4cb12c132395c4f03530db3096
Sourabh Swain <sourabhswain@gmail.com>
2015-08-12 09:29:44 +0530

    Create BuildConfig.java

---
+++ BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sunny_red_devil.footballstadiumslivewallpaper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
